15ad012f1a84b9b1dc6f3dcc14e246402604fc0e
dldmswl53 <dldmswl53@naver.com>
2020-11-10 22:33:43 +0900

    201110 집

---
+++ !웹디자인기능사/html-롯데신입채용/dist/js/comm.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.top-slide').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        dots: true
+      });
+  });
