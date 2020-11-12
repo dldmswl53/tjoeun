5c008fd4f9d80131c2b6ff1a06406fc1b52db491
dldmswl53 <dldmswl53@naver.com>
2020-11-12 13:48:45 +0900

    201112

--- !웹디자인기능사/201111-대한투어/js/comm.js
+++ !웹디자인기능사/201111-대한투어/js/comm.js
@@ -8,7 +8,7 @@
     })
 
     $(function(){
-        $('slideshow').each(function(){
+        $('.slideshow').each(function(){
             let $슬라이드이미지 = $(this).find('img'),
             슬라이드갯수 = $슬라이드이미지.length,
             현재이미지 = 0
@@ -17,12 +17,23 @@
                 setInterval(다음슬라이드,3000));
             
             function 다음슬라이드(){
-                let 다음이미지 = 현재이미지 + 1
+                let 다음이미지 = (현재이미지 + 1) % 슬라이드갯수
     
                 $슬라이드이미지.eq(현재이미지).fadeOut()
     
                 $슬라이드이미지.eq(다음이미지).fadeIn()
+                
+                현재이미지 = 다음이미지;
             }
+    })
+    $(function(){
+        $(".col2 img:eq(0)").click(function () {
+            $("#modal .inner").show();
+        })
+        // modal button을 클릭했을 때
+        $("#modal button").click(function () {
+            $("#modal").hide();
+        })
     })
 
 
